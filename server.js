554092fa456a7afd655a630442ac9a273a52d3a8
OlaFawole <Oladotun.Fawole@eurostar.com>
2020-04-26 20:31:33 +0100

    updating databae

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const image    = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
+      host : 'postgresql-colorful-55052',
       user : 'oladotun',
       password : '',
       database : 'smart-brain'
